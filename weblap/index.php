6b2220b09baf3645b4468c3da86d2346f99f9202
Domokos David <domokosdavid4@gmail.com>
2022-02-15 11:48:34 +0100

    mappába rendezve

---
+++ weblap/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  <title>Kreatív Ötletcentrum | Főoldal</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link href='https://fonts.googleapis.com/css?family=Alegreya Sans SC' rel='stylesheet'>
+  
+  <link rel="icon" href="photos/favicon.ico" type="image/gif" sizes="16x16">
+  <link rel="stylesheet" href="style.css">
+  
+
+
+</head>
+
+<body>
+  <!--Menü kezdete-->
+  <nav>
+    <div class="menu-icon">
+      <span class="fas fa-bars"></span>
+    </div>
+
+
+    <div class="nav-items">
+      <li><a href="index.php">Főoldal</a></li>
+      <li><a href="termekek.php">Termékek</a></li>
+      <li><a href="#">Rólunk</a></li>
+      <li><a href="#">Kapcsolat</a></li>
+    </div>
+    
+
+    <div class="search-icon">
+      <span class="fas fa-search"></span>
+    </div>
+
+    <div class="cancel-icon">
+      <span class="fas fa-times"></span>
+    </div>
+
+    <form action="#" id="kereso_mezo">
+      <input type="search" class="search-data" placeholder="Keresés" required>
+      <button type="submit" class="fas fa-search"></button>
+    </form>
+
+    <div class="bag-icon">
+      <a href="chart.php"><button type="button" class="btn btn"><span class="bi bi-bag"></span></button></a>
+    </div>
+
+
+  </nav>
+
+  <script type="text/javascript" src="main.js"></script>
+  <!--Menü vége-->
+
+
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident corporis quia possimus unde doloribus laborum enim perferendis. Blanditiis enim pariatur error, ducimus quam possimus repellendus, illum velit quisquam voluptatem perspiciatis.</p>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident corporis quia possimus unde doloribus laborum enim perferendis. Blanditiis enim pariatur error, ducimus quam possimus repellendus, illum velit quisquam voluptatem perspiciatis.</p>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident corporis quia possimus unde doloribus laborum enim perferendis. Blanditiis enim pariatur error, ducimus quam possimus repellendus, illum velit quisquam voluptatem perspiciatis.</p>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident corporis quia possimus unde doloribus laborum enim perferendis. Blanditiis enim pariatur error, ducimus quam possimus repellendus, illum velit quisquam voluptatem perspiciatis.</p>
+ 
+
+  <!--Footer eleje-->
+  <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>Információk</h4>
+  	 			<ul>
+  	 				<li><a href="#">Kapcsolat</a></li>
+  	 				<li><a href="#">Adatvédelem</a></li>
+  	 				<li><a href="#">Általános Szerződési Feltételek (ÁSZF)</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Segítség</h4>
+  	 			<ul>
+  	 				<li><a href="#">FAQ</a></li>
+  	 				<li><a href="#">Szállítás</a></li>
+  	 				<li><a href="#">Garancia</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>online shop</h4>
+  	 			<ul>
+  	 				<li><a href="#">watch</a></li>
+  	 				<li><a href="#">bag</a></li>
+  	 				<li><a href="#">shoes</a></li>
+  	 				<li><a href="#">dress</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>kövess minket</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
+ <!--Footer vége-->
+</body>
+
+</html>
