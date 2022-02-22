6dfd1d6542e400ad5a55e37a718a5f1772f6a90c
Domokos David <domokosdavid4@gmail.com>
2022-02-22 19:20:19 +0100

    google terkep beszurva

--- weblap/index.php
+++ weblap/index.php
@@ -78,6 +78,21 @@
     </div>
   </div>
 
+  <div class="container" id="elerhetoseg">
+    <div class="row">
+
+      <div class="col-lg-6">
+        <h2>Elérhetőségeink</h2>
+        <p>Email: kocentrum@info.hu</p>
+        <p>Telefonszám: +36301234567</p>
+      </div>
+      <div class="col-lg-6">
+        <iframe id="terkep" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9081.089942961125!2d19.397610731155908!3d47.41537022726169!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741be50def15e91%3A0x400c4290c1e29d0!2zR3nDtm1yxZEsIDIyMzA!5e0!3m2!1shu!2shu!4v1645550595304!5m2!1shu!2shu" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      </div>
+    </div>
+
+  </div>
+
 
 
   <!--Footer eleje-->
